866e8fdd52db458f93af8ddc4c453a7e5e4e267d
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-03-28 19:01:31 +0100

    fix meshes

--- data-raw/palettes.R
+++ data-raw/palettes.R
@@ -10,3 +10,10 @@ brain_pals = list(
   icbm = setNames(k$colour, k$region)
 )
 usethis::use_data(brain_pals, internal = TRUE, overwrite = TRUE)
+
+
+
+devtools::load_all("../../ggsegExtra/")
+icbm_3d <- ggsegExtra:::restruct_old_3datlas(icbm_3d)
+icbm_3d <- as_ggseg3d_atlas(icbm_3d)
+usethis::use_data(icbm_3d, internal = FALSE, overwrite = TRUE)
