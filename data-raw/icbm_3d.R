35b7332b033af753ec7e69219a565494baff3c28
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2023-05-05 11:59:30 +0200

    cleanup raw

--- data-raw/icbm_3d.R
+++ data-raw/icbm_3d.R
@@ -1,34 +1,35 @@
 library(tidyverse)
 library(ggseg)
+library(ggseg3d)
 
 folder = "data-raw/mesh3d/JHU_ICBM.fsaverage5/"
-mesh = lapply(list.files(folder, pattern="ply", full.names = T), 
+mesh = lapply(list.files(folder, pattern="ply", full.names = T),
               geomorph::read.ply, ShowSpecimen = F)
 
-annots = read_csv2(paste0(folder, "annot2filename.csv")) %>% 
+annots = read_csv2(paste0(folder, "annot2filename.csv")) %>%
   rename(files=filename,
-         area2 = JHU_label) %>% 
-  select(-1, -3) %>% 
+         area2 = JHU_label) %>%
+  select(-1, -3) %>%
   mutate(colour = ggtern::rgb2hex(R,G,B),
          area = gsub(" L$| R$", "", area2),
          label = annot,
-         annot = gsub("rh_|lh_", "", annot)) %>% 
-  select(-R, -G, -B) %>%   
+         annot = gsub("rh_|lh_", "", annot)) %>%
+  select(-R, -G, -B) %>%
   mutate(hemi = "subcort",
          surf = "LCBC"
-         
-  ) %>% 
-  separate(files, into=c(NA, NA, "roi"), sep="_") %>% 
+
+  ) %>%
+  separate(files, into=c(NA, NA, "roi"), sep="_") %>%
   mutate(colour = ifelse(area == lead(area), lead(colour), colour),
-         colour = ifelse(is.na(colour), lag(colour), colour)) %>% 
+         colour = ifelse(is.na(colour), lag(colour), colour)) %>%
   select(-area2)
 
 
 ff <- tibble(files = list.files(folder, pattern="ply", full.names = F),
-             atlas = "icbm_3d") %>% 
-  separate(files, sep="_", into=c(NA, NA, "roi"), remove = F) %>% 
-  separate(roi, sep="[.]", into=c("roi", NA), remove = F) %>% 
-  left_join(annots) 
+             atlas = "icbm_3d") %>%
+  separate(files, sep="_", into=c(NA, NA, "roi"), remove = F) %>%
+  separate(roi, sep="[.]", into=c("roi", NA), remove = F) %>%
+  left_join(annots)
 
 
 ff$mesh = list(vb=1)
@@ -38,11 +39,24 @@ for(i in 1:length(mesh)){
   )
 }
 
-icbm_3d <- ff %>% 
+icbm_3d <- ff %>%
   group_by(atlas, surf, hemi) %>%
-  nest() %>% 
+  nest() %>%
   as_ggseg3d_atlas()
 
-ggseg3d(atlas=icbm_3d, glassbrain = .6, glassbrain_hemisphere = "left")
+icbm_3d_orig <- icbm_3d
+
+icbm_3d <- icbm_3d_orig |>
+  unnest(cols = "ggseg_3d") |>
+  mutate(
+    label = if_else(
+      region == "Superior corona radiata",
+      gsub("acr", "scr", label),
+      label
+    )
+  ) |>
+  as_ggseg3d_atlas()
+
+ggseg3d::ggseg3d(atlas=icbm_3d, glassbrain = .6, glassbrain_hemisphere = "left")
 
 usethis::use_data(icbm_3d, overwrite = TRUE, internal = FALSE, compress = "xz")
--- data-raw/palettes.R
+++ data-raw/palettes.R
@@ -17,3 +17,36 @@ devtools::load_all("../../ggsegExtra/")
 icbm_3d <- ggsegExtra:::restruct_old_3datlas(icbm_3d)
 icbm_3d <- as_ggseg3d_atlas(icbm_3d)
 usethis::use_data(icbm_3d, internal = FALSE, overwrite = TRUE)
+
+
+# make hex ----
+p <- ggseg(atlas = icbm,
+           hemi = "left",
+           view = "lateral",
+           show.legend = FALSE,
+           colour = "grey30",
+           size = .2,
+           mapping = aes(fill =  region)) +
+  scale_fill_brain2(palette = atlas$palette) +
+  theme_void() +
+  hexSticker::theme_transparent()
+
+lapply(c("png"), function(x){
+  hexSticker::sticker(p,
+                      package = "ggsegSmith",
+                      filename = sprintf("man/figures/logo.%s", x),
+                      s_y = 1.2,
+                      s_x = 1,
+                      s_width = 1.5,
+                      s_height = 1.5,
+                      p_family = "mono",
+                      p_size = 10,
+                      p_color = "grey30",
+                      p_y = .6,
+                      h_fill = "white",
+                      h_color = "grey30"
+  )
+
+})
+
+pkgdown::build_favicons(overwrite = TRUE)
